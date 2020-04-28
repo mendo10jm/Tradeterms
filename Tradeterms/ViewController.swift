5f7b13e913c2daeb06f153858e6cff0c146f0a7b
Jose Manuel Mendoza <mendozamarin@gmail.com>
2020-04-28 10:29:19 +0200

    Initial Commit

---
+++ Tradeterms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tradeterms
+//
+//  Created by Jose Manuel Mendoza Marín on 28/04/2020.
+//  Copyright © 2020 JoseManuelYPablo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
